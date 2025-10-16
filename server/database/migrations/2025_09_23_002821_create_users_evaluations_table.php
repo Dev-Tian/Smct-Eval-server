a12c8f28034752fdc36d997e2d7090676eee0896
Cristian <smct.cristiansimbajon@gmail.com>
2025-10-16 15:10:47 +0800

    q

--- server/database/migrations/2025_09_23_002821_create_users_evaluations_table.php
+++ server/database/migrations/2025_09_23_002821_create_users_evaluations_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\QuarterUsersEvaluaion;
 use App\Models\QuarterUsersEvaluation;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
--- server/database/migrations/2025_10_14_060213_create_job_knowledge_table.php
+++ server/database/migrations/2025_10_14_060213_create_job_knowledge_table.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Models\Submission;
-use App\Models\UsersEvaluaion;
+
 use App\Models\UsersEvaluation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
