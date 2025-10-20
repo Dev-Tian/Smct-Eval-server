dbf53887c01334c4680502ffcd1ed227bba4094e
Cristian <smct.cristiansimbajon@gmail.com>
2025-10-20 09:07:19 +0800

    added auto hash password

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -10,9 +10,36 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasRoles;
-
+    use HasFactory, HasRoles, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
     public function branches()
     {
