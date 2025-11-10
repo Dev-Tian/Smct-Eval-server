f3d396d048ae763b820321b6f40b2bda22cc9f8b
Cristian <smct.cristiansimbajon@gmail.com>
2025-11-10 16:46:05 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -29,25 +29,29 @@ Route::get('/profile', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(
     function () {
-        Route::controller(UserController::class)->group(function () {
-            Route::get('users', 'getAllUsers');
-            Route::get('getAll_Active_users', 'getAllActiveUsers');
-            Route::get('getAll_Pending_users', 'getAllPendingUsers');
-            Route::get('getAllSuspendedUsers', 'getAllSuspendedUsers');
-            Route::get('getAllReinstatedUsers', 'getAllReinstatedUsers');
-            Route::get('show_user/{user}', 'showUser');
-            Route::post('update_user/{user}', 'updateUser');
-            Route::post('upload_avatar', 'uploadAvatar');
-            Route::post('update_employee_auth', 'updateProfileUserAuth');
-            Route::post('delete_user/{user}', 'deleteUser');
-            Route::post('approveRegistration/{user}', 'approveRegistration');
-            Route::post('rejectRegistration/{user}', 'rejectRegistration');
-        });
+        Route::controller(UserController::class)->group(
+            function () {
+                Route::get('users', 'getAllUsers');
+                Route::get('getAll_Active_users', 'getAllActiveUsers');
+                Route::get('getAll_Pending_users', 'getAllPendingUsers');
+                Route::get('getAllSuspendedUsers', 'getAllSuspendedUsers');
+                Route::get('getAllReinstatedUsers', 'getAllReinstatedUsers');
+                Route::get('show_user/{user}', 'showUser');
+                Route::post('update_user/{user}', 'updateUser');
+                Route::post('upload_avatar', 'uploadAvatar');
+                Route::post('update_employee_auth', 'updateProfileUserAuth');
+                Route::post('delete_user/{user}', 'deleteUser');
+                Route::post('approveRegistration/{user}', 'approveRegistration');
+                Route::post('rejectRegistration/{user}', 'rejectRegistration');
+            }
+        );
 
-        Route::controller(SuspensionController::class)->group(function(){
-            Route::post('suspend/{user}', 'store');
-            Route::post('updateSuspension/{suspension}', 'update');
-        } );
+        Route::controller(SuspensionController::class)->group(
+            function () {
+                Route::post('suspend/{user}', 'store');
+                Route::post('updateSuspension/{suspension}', 'update');
+            }
+        );
 
         Route::post('logout', function (Request $request) {
             auth()->guard('web')->logout();
