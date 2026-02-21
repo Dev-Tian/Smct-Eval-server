ebefc51913a4c750b5a14acec0c014eda0de798d
Cristian <smct.cristiansimbajon@gmail.com>
2026-02-21 17:13:17 +0800

    updated

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -147,16 +147,15 @@ class UserController extends Controller
             ], 404);
         }
 
-        return match($user->is_active){
-            'pending'       =>
-                response()->json([
-                    "message"   => "Your account is not activated yet. Please wait for admin to approve."
-                ], 401),
-            'declined'      =>
-                 response()->json([
-                    "message"   => "Your account has been rejected."
-                ], 401)
-        };
+        if($user->is_active === 'pending' ){
+            return response()->json([
+                "message"   => "Your account is not activated yet. Please wait for admin to approve."
+            ], 401);
+        }elseif($user->is_active === 'declined'){
+           return response()->json([
+                "message"   => "Your account has been rejected."
+            ], 401);
+       }
 
         $credentials = [
             'username' => !filter_var($request->email, FILTER_VALIDATE_EMAIL) ? $request->email : $user->username,
