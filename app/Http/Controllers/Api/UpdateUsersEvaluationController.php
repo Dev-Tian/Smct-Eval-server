f173e664908b769ac720d9af48ef3a2aa0ffede8
Cristian <smct.cristiansimbajon@gmail.com>
2026-07-09 16:04:05 +0800

    Update UpdateUsersEvaluationController.php

--- app/Http/Controllers/Api/UpdateUsersEvaluationController.php
+++ app/Http/Controllers/Api/UpdateUsersEvaluationController.php
@@ -21,10 +21,10 @@ class UpdateUsersEvaluationController extends Controller
     {
         return match (true) {
 
-            $usersEvaluation->rejected_by_id == $usersEvaluation->approver2_id && !empty($usersEvaluation->rejected_by_id)
+            $usersEvaluation->rejected_by_id == $usersEvaluation->approver2_id && !empty($usersEvaluation->approver2_id)
                 => EvalStatus::pending_approval_2,
 
-            $usersEvaluation->rejected_by_id == $usersEvaluation->approver1_id || !empty($usersEvaluation->approver1_id)
+            ($usersEvaluation->rejected_by_id == $usersEvaluation->approver1_id && !empty($usersEvaluation->approver1_id)) || !empty($usersEvaluation->approver1_id)
                 => EvalStatus::pending_approval_1,
 
             default => EvalStatus::pending,
