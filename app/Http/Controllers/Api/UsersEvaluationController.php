34f94cafd7b801ccf7b2e66a76b875bcacfcf62b
Cristian <smct.cristiansimbajon@gmail.com>
2026-07-21 14:24:28 +0800

    Update UsersEvaluationController.php

--- app/Http/Controllers/Api/UsersEvaluationController.php
+++ app/Http/Controllers/Api/UsersEvaluationController.php
@@ -260,6 +260,7 @@ class UsersEvaluationController extends Controller
                     "employee_branch_code",
                     "rating",
                     "status",
+                    "noteIfRejected",
                     "reviewTypeProbationary",
                     "reviewTypeRegular",
                     "reviewTypeOthersImprovement",
